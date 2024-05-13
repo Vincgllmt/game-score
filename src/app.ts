e9daaeaabb31c13102e2657084658023c84bed08
Alexis Udycz <alexis.udycz@etudiant.univ-reims.fr>
2024-05-13 06:48:07 +0000

    GREEN: Ajout de la route '/api/hello/world'

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express';
-        
+
 export const app = express();
+
+app.get('/api/hello/world', (req: Request, res: Response, next: NextFunction) => {
+    res.send(JSON.stringify({ message: 'Hello, world!' }));
+});
