b79e8844b414fe8f3d3fc0bcf40576874ee8be96
Alexis Udycz <alexis.udycz@etudiant.univ-reims.fr>
2024-05-13 07:20:53 +0000

    GREEN: Ajout du test du body de hello/square/5

--- tests/hello/hello.test.ts
+++ tests/hello/hello.test.ts
@@ -10,5 +10,6 @@ describe('Test /api/hello', () => {
     test('GET /api/hello/square/5', async () => {
         const response = await supertest(app).get('/api/hello/square/5');
         expect(response.statusCode).toBe(200);
+        expect(response.body).toStrictEqual({ result: 25 });
     });
 });
