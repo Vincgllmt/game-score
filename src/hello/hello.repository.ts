bdbe5b5d94ef6d27ff778ffeca5762c4ad68dfe0
Alexis Udycz <alexis.udycz@etudiant.univ-reims.fr>
2024-05-13 09:09:32 +0000

    REFACTOR: Ajout de la classe HelloRepository

--- src/hello/hello.repository.ts
+++ src/hello/hello.repository.ts
@@ -3,3 +3,15 @@ import { mongodb } from "../services/mongodb";
 import { HelloData } from "./hello";
         
 export const helloCollection = mongodb.collection<HelloData>('hello');
+
+export class HelloRepository {
+    async clear() {
+        return await helloCollection.deleteMany({});
+    }
+
+    async insert(...data: HelloData[]) {
+        return await helloCollection.insertMany(data);
+    }
+}
+
+export const helloRepository = new HelloRepository();
