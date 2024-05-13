4fa7fbe607d3ca8b11cd065dfc8dc0532146e964
Alexis Udycz <alexis.udycz@etudiant.univ-reims.fr>
2024-05-13 07:54:15 +0000

    feat: :heavy_plus_sign: Ajout de swagger-ui-express et yaml

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import { app } from './app';
+import { serve, setup } from 'swagger-ui-express';
+import { parseDocument } from 'yaml';
+import { readFileSync } from 'fs';
+
+app.use('/api_doc', serve, setup(parseDocument(readFileSync("./openapi.yml").toString())));
 
 app.listen(7777, () => {
     console.log('Serveur local démarré : http://localhost:7777');
