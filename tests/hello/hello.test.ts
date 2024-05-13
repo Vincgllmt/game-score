8cb82a4d93ffa29a50ee54be427b11424baecd6f
Alexis Udycz <alexis.udycz@etudiant.univ-reims.fr>
2024-05-13 07:03:25 +0000

    RED: ajout test sur square/5

--- tests/hello/hello.test.ts
+++ tests/hello/hello.test.ts
@@ -7,4 +7,8 @@ describe('Test /api/hello', () => {
         expect(response.statusCode).toBe(200);
         expect(response.body).toStrictEqual({ message: 'Hello, world!' });
     });
+    test('GET /api/hello/square/5', async () => {
+        const response = await supertest(app).get('/api/hello/square/5');
+        expect(response.statusCode).toBe(200);
+    });
 });
