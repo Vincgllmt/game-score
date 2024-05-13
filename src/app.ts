94f0a0f4c46e29f3eb4c6df6700f81821ef58efd
Alexis Udycz <alexis.udycz@etudiant.univ-reims.fr>
2024-05-13 06:58:51 +0000

    REFACTOR: Correction du code sans le JSON.stringify (oups)

--- src/app.ts
+++ src/app.ts
@@ -3,5 +3,5 @@ import express, { Request, Response, NextFunction } from 'express';
 export const app = express();
 
 app.get('/api/hello/world', (req: Request, res: Response, next: NextFunction) => {
-    res.send(JSON.stringify({ message: 'Hello, world!' }));
+    res.send({ message: 'Hello, world!' });
 });
