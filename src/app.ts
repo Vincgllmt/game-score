76de44282ab974f2686792c9bddb26616bc83e7c
Alexis Udycz <alexis.udycz@etudiant.univ-reims.fr>
2024-05-24 06:34:01 +0000

    REFACTOR: Ajout de la gestion d'erreur

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { Request, Response, NextFunction } from 'express';
 import helloRouter  from './hello/hello.router';
 import playerRouter from './player/player.router';
 import gameRouter from './game/game.router';
@@ -9,4 +10,10 @@ app.use(express.json());
 app.use("/", helloRouter)
 app.use("/", playerRouter);
 app.use("/", gameRouter);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.log("ERREUR :", err);
+    res.status(500).send({ error: 'Something went wrong!' });
+});
+
 export {app};
